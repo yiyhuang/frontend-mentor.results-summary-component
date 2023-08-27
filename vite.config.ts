a90c160202a060e0933b868545cfb56368202f41
Yiyang Huang <56028891+yiyhuang@users.noreply.github.com>
2023-08-27 23:42:41 +0900

    build: Add base for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/frontend-mentor.results-summary-component/', // for Github Pages deployment
   plugins: [react()],
-})
+});
